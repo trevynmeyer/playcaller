6df94e3b2755957f0ec9acbdfbc99196a8368825
trevynmeyer <trevyn.meyer@gmail.com>
2021-03-23 13:05:57 -0600

    new file

--- index.php
+++ index.php
@@ -12,56 +12,41 @@
   </head>
   <body>
 
+<?php
+$content = file_get_contents('https://docs.google.com/spreadsheets/d/1uoEYbuYBpHDi4iPTNhf6Z1m-aSBWUo_MocINnfv2BzY/export?format=csv');
+echo "";
+print_r($content);
+echo "";
+?>
+
+
+
   <div class="container">
-  <div class="row">
-    <div class="col-sm">
-      One of three columns
-    </div>
-    <div class="col-sm">
-      One of three columns
-    </div>
-    <div class="col-sm">
-      One of three columns
-    </div>
-  </div>
-</div>
 
+    <h2>Offense></h2>
 
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Plays</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">
 
-  <form>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Email address</label>
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    </textarea>
   </div>
+
   <div class="form-group">
-    <label for="exampleFormControlSelect1">Example select</label>
-    <select class="form-control" id="exampleFormControlSelect1">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-    </select>
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlSelect2">Example multiple select</label>
-    <select multiple class="form-control" id="exampleFormControlSelect2">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-    </select>
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlTextarea1">Example textarea</label>
+    <label for="exampleFormControlTextarea1">Escential Players</label>
     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
   </div>
+
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Skill Players</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+
+
   <button type="submit" class="btn btn-primary">Sign in</button>
 </form>
 
-
-
+</div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
