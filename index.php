0e69cce8aa47b1c49bc9d849c7711da3082c9e3e
trevynmeyer <trevyn.meyer@gmail.com>
2021-03-23 12:49:11 -0600

    new file

--- index.php
+++ index.php
@@ -12,7 +12,19 @@
   </head>
   <body>
 
-
+  <div class="container">
+  <div class="row">
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+  </div>
+</div>
 
 
 
